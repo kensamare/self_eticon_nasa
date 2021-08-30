3396666bd66062d9dcfb91640f572cd460957a91
Andrew Kurakov <sniperaft@gmail.com>
2021-08-30 21:39:54 +0700

    Доработка

--- lib/screens/detail_screen/detail_screen.dart
+++ lib/screens/detail_screen/detail_screen.dart
@@ -17,7 +17,180 @@ class DetailScreen extends StatelessWidget {
       body: SingleChildScrollView(
         child: Column(
           children: [
-            Hero(tag: '${photo.camera!.name!} ${photo.id}' ,child: Container(width: MediaQuery.of(context).size.width, height: 250, child: Image.network(photo.imgSrc!, fit: BoxFit.fitWidth,),)),
+            Hero(
+                tag: '${photo.camera!.name!} ${photo.id}',
+                child: Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: 250,
+                  child: Image.network(
+                    photo.imgSrc!,
+                    fit: BoxFit.fitWidth,
+                  ),
+                ),
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width,
+            padding: EdgeInsets.all(15),
+              margin: EdgeInsets.fromLTRB(15, 30, 15, 15),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: Colors.white,
+                boxShadow: [BoxShadow(offset: Offset(0,5), color: Colors.black.withOpacity(0.3), blurRadius: 10)],
+              ),
+              child: Column(
+                children: [
+                  Text(
+                    'Camera Info',
+                    style: TextStyle(
+                      fontWeight: FontWeight.w800,
+                      fontSize: 18,
+                    ),
+                  ),
+                  SizedBox(height: 10,),
+                  Row(
+                    children: [
+                      Text(
+                        'Id: ',
+                        style: TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                      Text(
+                        '${photo.camera!.id}',
+                        style: TextStyle(
+                          fontSize: 15,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        'Full name: ',
+                        style: TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                      Text(
+                        '${photo.camera!.fullName}',
+                        style: TextStyle(
+                          fontSize: 15,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        'Rover id: ',
+                        style: TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                      Text(
+                        '${photo.camera!.roverId}',
+                        style: TextStyle(
+                          fontSize: 15,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              padding: EdgeInsets.all(15),
+              margin: EdgeInsets.fromLTRB(15, 0, 15, 15),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: Colors.white,
+                boxShadow: [BoxShadow(offset: Offset(0,5), color: Colors.black.withOpacity(0.3), blurRadius: 10)],
+              ),
+              child: Column(
+                children: [
+                  Text(
+                    'Rover Info',
+                    style: TextStyle(
+                      fontWeight: FontWeight.w800,
+                      fontSize: 18,
+                    ),
+                  ),
+                  SizedBox(height: 10,),
+                  Row(
+                    children: [
+                      Text(
+                        'Name: ',
+                        style: TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                      Text(
+                        '${photo.rover!.name}',
+                        style: TextStyle(
+                          fontSize: 15,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        'Launch date: ',
+                        style: TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                      Text(
+                        '${photo.rover!.launchDate}',
+                        style: TextStyle(
+                          fontSize: 15,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        'Landing date: ',
+                        style: TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                      Text(
+                        '${photo.rover!.landingDate}',
+                        style: TextStyle(
+                          fontSize: 15,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        'Status: ',
+                        style: TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                      Text(
+                        '${photo.rover!.status}',
+                        style: TextStyle(
+                          fontSize: 15,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
